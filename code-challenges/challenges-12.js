5f3ba35b4cfd891da6a4c9ac47243b31804dd0e0
yahia199 <yahiakhlil0151400@gmail.com>
2022-02-22 02:03:23 +0200

    solve 2 exercise12

--- code-challenges/challenges-12.js
+++ code-challenges/challenges-12.js
@@ -105,13 +105,10 @@ const cvsFiltration = (arr) => {
 const vowelsFiltration = (arr) => {
   // write your code here
 
-  let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!arr[i].match(/[aeiou]/g)) {
-      result.push(arr[i]);
-    }
-  }
-  return result;
+  const vowel = arr.filter((word) => {
+    return !word.match(/[aeiou]/g);
+  });
+  return vowel;
 };
 // -------------------------------------------------------------------------------------------------------
 
